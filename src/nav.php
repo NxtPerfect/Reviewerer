91a44d8cea0e52e703444dbd67adc42246b0bc35
NxtPerfect <alakaxan@protonmail.com>
2024-06-19 12:07:26 +0200

    Minor css updates
    added showcase screenshots

--- src/nav.php
+++ src/nav.php
@@ -11,13 +11,13 @@
   }
 ?>
 
-<nav class="bg-neutral-200 w-full py-4">
+<nav class="grid grid-col-3 justify-items-center max-sm:justify-between max-sm:px-0 justify-center items-center gap-4 bg-neutral-200 w-full py-4">
   <div class="logo">Reviewerer</div>
-  <form class="bg-black rounded-full pl-4" action="search.php">
-    <input class="bg-transparent text-white" type="text" name="search_name" maxlength="64" placeholder="Wyszukaj nazwy produktu"></input>
-    <button class="text-white bg-gray-600 hover:bg-gray-700 hover:cursor-pointer active:bg-gray-500 transition rounded-r-full px-2 pr-4 py-1 h-full" type="submit">Szukaj</button>
+  <form class="flex flex-row bg-black rounded-full pl-4" action="search.php">
+    <input class="bg-transparent text-white w-full" type="text" name="search_name" maxlength="64" placeholder="Wyszukaj nazwy produktu"></input>
+    <button class="text-white bg-gray-600 hover:bg-gray-700 hover:cursor-pointer active:bg-gray-500 transition rounded-r-full xl:px-2 xl:pr-4 xl:py-1 h-full" type="submit">Szukaj</button>
   </form>
-  <div class="flex flex-row gap-4">
+  <div class="flex xl:flex-row flex-col gap-4">
   <?php
     if (isset($_SESSION["email"])) {
       echo "<a class='text-sky-500 hover:text-sky-600 hover:cursor-pointer active:text-sky-400 transition underline' href='/src/home.php'>Strona główna</a>";
